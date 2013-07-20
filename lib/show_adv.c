ef4234d8601d4ec9d90f5e3ce7e5c455fc09cac0
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2013-07-20 22:56:15 +0300

    Added a minimalist CMake-based build framework.

--- lib/show_adv.c
+++ lib/show_adv.c
@@ -1,8 +1,8 @@
-#include <graphics.h>
+//#include <graphics.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
+#include <curses.h>
 #include <math.h>
 
 #include "geom.h"
